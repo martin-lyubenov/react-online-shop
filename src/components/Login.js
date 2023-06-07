13ae534b05ff11e743373380906bf1dd81cc331b
Martin Lyubenov <martindlyubenov@gmail.com>
2023-06-07 11:52:40 +0300

    login form build and styled (no logic yet)

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,28 @@
+
+import { Link } from 'react-router-dom';
+import classes from './Login.module.css'
+
 function Login(params) {
-    return <h1>Login</h1>
+  return (
+    <section >
+      <div className={classes.form}>
+        <h2>Login</h2>
+        <form >
+          <input type="text" name="email" id="email" placeholder="email" />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="password"
+          />
+          <button type="submit">login</button>
+          <p className={classes.message} >
+            Not registered? <Link to="/register">Create an account</Link>
+          </p>
+        </form>
+      </div>
+    </section>
+  );
 }
 
-export default Login
+export default Login;
