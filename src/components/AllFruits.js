8038cdcb7cce83dc3855b3d05c224b5b5af6b1a7
Martin Lyubenov <martindlyubenov@gmail.com>
2023-06-08 12:44:54 +0300

    loader added to all fruits ( and it even works!)

--- src/components/AllFruits.js
+++ src/components/AllFruits.js
@@ -1,10 +1,36 @@
+import { useLoaderData } from "react-router-dom";
+
+const FruitCard = ({fruit}) => {
+    console.log(fruit);
+  return (
+    <div class="fruit">
+      <img src={fruit.imageUrl} alt="example1" />
+      <h3 class="title">{fruit.name}</h3>
+      <p class="description">{fruit.description}</p>
+      <a class="details-btn" href="/dashboard/${fruit._id}">
+        More Info
+      </a>
+    </div>
+  );
+};
+
 function AllFruits(params) {
-    return <h1>All Products</h1>
+  const data = useLoaderData();
+  const allFruits = data.results;
+
+  const content =
+    allFruits && allFruits.length > 0 ? (
+      allFruits.map((fruit) => <FruitCard fruit={fruit} />)
+    ) : (
+      <p>No fruits yet</p>
+    );
+
+  return (
+    <section>
+      <h1>All Products</h1>
+      {content}
+    </section>
+  );
 }
-
-
-
-
-
 
 export default AllFruits;
--- src/pages/AllFruits.js
+++ src/pages/AllFruits.js
@@ -1,7 +1,23 @@
+import { json } from "react-router-dom";
 import AllFruits from "../components/AllFruits";
+import { get } from "../data/api";
+import { endpoints } from "../util/endpoints";
 
 function AllFruitsPage(params) {
   return <AllFruits />;
+}
+
+export async function loader(params) {
+  try {
+    const data = await get(endpoints.allFruits);
+
+    return data;
+  } catch (error) {
+    throw json(
+      { message: "Something went wrong, please reload the page" },
+      { status: 500 }
+    );
+  }
 }
 
 export default AllFruitsPage;
