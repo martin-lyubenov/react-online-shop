b1a38300bc0c07b7a47dd81142bfc2bbe651bc0f
Martin Lyubenov <martindlyubenov@gmail.com>
2023-06-08 15:04:39 +0300

    moved fruits components to fruits dir

--- src/components/MyProducts.js
+++
@@ -1,5 +0,0 @@
-function MyProducts(params) {
-    return <h1>My Products</h1>
-}
-
-export default MyProducts;
--- src/pages/MyProducts.js
+++
@@ -1,7 +0,0 @@
-import MyProducts from "../components/MyProducts";
-
-function MyProductsPage(params) {
-  return <MyProducts />;
-}
-
-export default MyProductsPage;
