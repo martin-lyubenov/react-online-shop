438335728b451c1e9c0f5b06de839c54dbca42cc
Martin Lyubenov <martindlyubenov@gmail.com>
2023-06-09 11:37:36 +0300

    refactored AddFruit action to used for Edit also

--- src/pages/AddFruit.js
+++ src/pages/AddFruit.js
@@ -1,45 +1,22 @@
 import { json, redirect } from "react-router-dom";
-import CreateForm from "../components/Forms/CreateForm";
+import CreateUpdateForm from "../components/Forms/CreateUpdateForm";
 import { post } from "../data/api";
 import { endpoints } from "../util/endpoints";
 import { getUserData } from "../util/util";
+import { formActions } from "../util/fromActions";
 
 function AddFruitPage(params) {
-  return <CreateForm />;
+  return <CreateUpdateForm method={"POST"} />;
 }
 
 export async function action({ request }) {
-  const formData = await request.formData();
-
-  // extracting data from the Create Form and trimming the date to avoid any unwanted spaces
-  const { name, imageUrl, description, nutrition } = Object.fromEntries(
-    [...formData].map(([k, v]) => [k, v.trim()])
-  );
-
-  const userData = getUserData();
-  const owner = {
-    __type: "Pointer",
-    className: "_User",
-    objectId: userData.objectId,
-  };
-
-  const response = await post(endpoints.allFruits, {
-    name,
-    imageUrl,
-    description,
-    nutrition,
-    owner,
-  });
-
-  if (response.ok === false) {
-    const error = await response.json();
+  const url = endpoints.allFruits;
+  try {
+    await formActions(request, post, url);
+    return redirect("/all-fruits");
+  } catch (error) {
     throw json({ message: error.error }, { status: error.code });
   }
-
-  // result is not needed but kept just in case, line can be removed
-  const result = await response.json();
-
-  return redirect("/all-fruits");
 }
 
 export default AddFruitPage;
---
+++ src/util/fromActions.js
@@ -0,0 +1,33 @@
+import { getUserData } from "./util";
+
+export async function formActions(request, httpReq, url) {
+  const formData = await request.formData();
+
+  // extracting data from the Create Form and trimming the date to avoid any unwanted spaces
+  const { name, imageUrl, description, nutrition } = Object.fromEntries(
+    [...formData].map(([k, v]) => [k, v.trim()])
+  );
+
+  const userData = getUserData();
+  const owner = {
+    __type: "Pointer",
+    className: "_User",
+    objectId: userData.objectId,
+  };
+
+  const response = await httpReq(url, {
+    name,
+    imageUrl,
+    description,
+    nutrition,
+    owner,
+  });
+
+  if (response.ok === false) {
+    const error = await response.json();
+    throw error;
+  }
+
+  // result is not needed but kept just in case, line can be removed
+  // const result = await response.json();
+}
