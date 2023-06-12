3ddab6637eadf450876d18ccb5c83acfffabe07f
Martin Lyubenov <martindlyubenov@gmail.com>
2023-06-12 12:11:52 +0300

    added price field and total price

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,14 +4,15 @@ import CartItem from "./CartItem";
 
 const Cart = (props) => {
   
-  const items = useSelector((state) => state.items.items);
+  const items = useSelector((state) => state.items);
+  const processedPrice = items.totalCost.toFixed(2);
 
   let content = <p>No items</p>;
 
-  if (items.length > 0) {
+  if (items.items.length > 0) {
     content = (
       <ul>
-        {items.map((item) => <CartItem key={item.id} item={item} />)}
+        {items.items.map((item) => <CartItem key={item.objectId} item={item} />)}
       </ul>
     );
   }
@@ -20,6 +21,7 @@ const Cart = (props) => {
     <div className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       {content}
+      {processedPrice}
     </div>
   );
 };
--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -2,30 +2,31 @@ import { itemsActions } from "../../store/item";
 import classes from "./CartItem.module.css";
 import { useDispatch } from "react-redux";
 
-const CartItem = (props) => {
-  const { title, quantity, total, price, id } = props.item;
+const CartItem = ({ item }) => {
+  const { name, nutrition, price, objectId, qty } = item;
+  console.log(item);
+  const processedPrice = Number(price).toFixed(2)
 
   const dispatch = useDispatch();
   const increaseHandler = () => {
-    dispatch(itemsActions.increaseQty({ title, quantity, total, price, id }));
+    dispatch(itemsActions.increaseQty({ name, nutrition, price, objectId }));
   };
 
   const decreaseHandler = () => {
-    dispatch(itemsActions.decreaseQty({ title, quantity, total, price, id }));
+    dispatch(itemsActions.decreaseQty({ name, nutrition, price, objectId }));
   };
 
   return (
     <li className={classes.item}>
       <header>
-        <h3>{title}</h3>
+        <h3>{name}</h3>
         <div className={classes.price}>
-          ${total.toFixed(2)}{" "}
-          <span className={classes.itemprice}>(${price.toFixed(2)}/item)</span>
+          <span className={classes.itemprice}>{processedPrice}/item</span>
         </div>
       </header>
       <div className={classes.details}>
         <div className={classes.quantity}>
-          x <span>{quantity}</span>
+          x <span>{qty}</span>
         </div>
         <div className={classes.actions}>
           <button onClick={decreaseHandler}>-</button>
