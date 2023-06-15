ded03a3147ad7b54d045d109a1c7315bd1adf0b4
Martin Lyubenov <martindlyubenov@gmail.com>
2023-06-15 11:22:36 +0300

    added main container component

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
 import MainNav from "../components/MainNav";
+import MainContainer from "../components/UI/MainContainer";
 
 function Layout(params) {
   return (
     <>
       <MainNav />
-      <Outlet />
+      <MainContainer>
+        <Outlet />
+      </MainContainer>
     </>
   );
 }
